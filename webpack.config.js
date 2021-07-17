8d9b703e4d9322c7ba4df84c7bfde779646d4881
17314FilipNikolic <filnik@elfak.rs>
2021-07-17 22:20:00 +0200

    Odradjeno formatiranje

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   experiments: {
-    topLevelAwait: true
-  }
+    topLevelAwait: true,
+  },
 };
